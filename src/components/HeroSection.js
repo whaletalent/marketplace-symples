09241dca9128b7b1fe6ae8458d4bd167886e781d
LuanFerreira <luanfc@gmail.com>
2019-07-09 19:03:03 -0300

    Inserção de Menu de navegação

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,57 +1,59 @@
-import React, {Component, Fragment} from 'react'
-import {Hero, Container, Title, Input, Tab, Column, Image} from 'rbx'
-import '../styles/HeroSection.scss'
-import Logo from '../assets/logo_symplifica.svg'
+import React, { Component, Fragment } from "react";
+import { Hero, Container, Title, Input, Tab, Column, Image } from "rbx";
+import "../styles/HeroSection.scss";
+import Logo from "../assets/logo_symplifica.svg";
+import { BrowserRouter as Router, Link } from "react-router-dom";
 
-
-export default class HeroSection extends Component{
-    state={
-        categories:[
-            'Cofee Break',
-            'Infraestrutura',
-            'Palestrantes',
-            'Locais',
-            'Brindes',
-            'Músicos ',
-            'Buffet'
-        ]
-    }
-    render(){
-        return(
-            <Fragment>
-                <Hero color="default-primary" className="heroFoot">
-                    <Hero.Body>
-                        <Container>
-                            <Column.Group centered>
-                                <Column size="one-quarter">
-                                    <Image class="logoFooter" size="1by3" src={Logo} />
-                                </Column>
-                            </Column.Group>
-                        <Input className="inputHero" type="text" color='default-primary' placeholder='Digite o que precisa' />
-                        <Title className="subtitle" size="3" subtitle >
-                            Tudo para seu evento
-                        </Title>
-                        </Container>
-                    </Hero.Body>
-                    <Hero.Foot >
-                        <Container>
-                            <Tab.Group as="nav" type="boxed" fullwidth>
-                            {
-                                this.state.categories.map( category => {
-                                    return(
-                                        // usar active para o item ativo do menu
-                                        <Tab className="categoria">
-                                            {category}
-                                        </Tab>
-                                    )
-                                })
-                            }
-                            
-                            </Tab.Group>
-                        </Container>
-                    </Hero.Foot>
-                </Hero>
-            </Fragment>
-        )
-    }
+export default class HeroSection extends Component {
+  state = {
+    categories: [
+      { name: "Cofee Break", link: "coffee-break" },
+      { name: "Infraestrutura", link: "infraestrutura" },
+      { name: "Palestrantes", link: "palestrantes" },
+      { name: "Locais", link: "locais" },
+      { name: "Brindes", link: "brindes" },
+      { name: "Músicos ", link: "musicos" },
+      { name: "Buffet", link: "buffet" }
+    ]
+  };
+  render() {
+    return (
+      <Fragment>
+        <Hero color="default-primary" className="heroFoot">
+          <Hero.Body>
+            <Container>
+              <Column.Group centered>
+                <Column size="one-quarter">
+                  <Image class="logoFooter" size="1by3" src={Logo} />
+                </Column>
+              </Column.Group>
+              <Input
+                className="inputHero"
+                type="text"
+                color="default-primary"
+                placeholder="Digite o que precisa"
+              />
+              <Title className="subtitle" size="3" subtitle>
+                Tudo para seu evento
+              </Title>
+            </Container>
+          </Hero.Body>
+          <Hero.Foot>
+            <Container>
+              <Tab.Group as="nav" type="boxed" fullwidth>
+                {this.state.categories.map(category => {
+                  return (
+                    // usar active para o item ativo do menu
+                    <a href={`/${category.link}`}>
+                        <Tab className="categoria">{category.name}</Tab>
+                    </a>
+                  );
+                })}
+              </Tab.Group>
+            </Container>
+          </Hero.Foot>
+        </Hero>
+      </Fragment>
+    );
+  }
 }
