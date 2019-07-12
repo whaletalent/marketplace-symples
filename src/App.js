b7f44f8c5e31bd1827b911b22734cdad81ff6512
EduardoGFreitas <egoncalvesfreitas@gmail.com>
2019-07-12 19:15:11 +0000

    implementação de service workers

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
-import React, {Fragment} from 'react';
-import './App.scss';
+import React, { Fragment, Component } from "react";
+import "./App.scss";
 
-import HeroSection from './components/HeroSection'
-import FooterCopyright from './components/FooterCopyright'
-import FooterMenu from './components/FooterMenu'
+import HeroSection from "./components/HeroSection";
+import FooterCopyright from "./components/FooterCopyright";
+import FooterMenu from "./components/FooterMenu";
 
+import OfflineAlert from "./components/OfflineAlert";
+import Routes from "./Routes";
 
+class App extends Component {
+  state = {
+    online: navigator.onLine
+  };
+  componentDidMount() {
+    window.addEventListener("online", this.handleNetworkChange);
+    window.addEventListener("offline", this.handleNetworkChange);
+  }
 
-import Routes from './Routes'
-
-function App() {
-  
-  return (
-    <div className="App">
-      <HeroSection></HeroSection>
-      <div className="content">
-        <Routes/>        
+  componentWillUnmount() {
+    window.removeEventListener("online", this.handleNetworkChange);
+    window.removeEventListener("offline", this.handleNetworkChange);
+  }
+  handleNetworkChange = () => {
+    this.setState({
+      online: navigator.onLine
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <HeroSection />
+        <div className="content">
+          <Routes />
+        </div>
+        <FooterMenu />
+        <FooterCopyright />
+        {!this.state.online && <OfflineAlert />}
       </div>
-      <FooterMenu/>
-      <FooterCopyright></FooterCopyright>
-    </div>
-  );
+    );
+  }
 }
-
 
 export default App;
---
+++ src/components/OfflineAlert/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./styles.scss";
+
+const OfflineAlert = () => (
+  <div className="offline-container">Você está offline </div>
+);
+
+export default OfflineAlert;
